d1280d884dd85178f02e97533eae613ecd6d0f7a
IVainqueur <ishimvainqueur@gmail.com>
2022-08-13 21:12:26 +0200

    checkup-c3b72ac047da429fb0e969f086ad5f15

--- src/components/MediaButtonsOverlay.jsx
+++ src/components/MediaButtonsOverlay.jsx
@@ -36,7 +36,7 @@ const MediaButtonsOverlay = ({ styles, mediaButtonClick, isPlaying, isLoading, _
 
             </div>
             <div className='absolute bottom-0 left-0 w-full'>
-                <span className='absolute'></span>
+                <span className='absolute -top-1 text-white'></span>
                 <progress
                     min={0}
                     value={Math.round(_time.current)}
@@ -53,7 +53,7 @@ const MediaButtonsOverlay = ({ styles, mediaButtonClick, isPlaying, isLoading, _
                         console.log(percentage, _time.max * percentage)
                     }}
                 ></progress>
-                <span className='absolute'></span>
+                <span className='absolute -top-1 text-white'></span>
             </div>
         </div>
     )
